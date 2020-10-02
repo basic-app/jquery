ce8b78f7085be342601eeff3b569cc3176fb48cb
basic-app <dev@basic-app.com>
2020-10-02 23:54:46 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -8,21 +8,23 @@ use BasicApp\Publisher\PublisherEvents;
 use BasicApp\Publisher\Events\PublishEvent;
 use BasicApp\JQuery\Config\JQuery;
 
-PublisherEvents::onPublish(function(PublishEvent $event)
-{
-    helper('path');
+PublisherEvents::onPublish(
+    function(PublishEvent $event)
+    {
+        helper('path');
 
-    $config = config(JQuery::class);
+        $config = config(JQuery::class);
 
-    $event->download(
-        'https://code.jquery.com/jquery-' . $config->version . '.min.js', 
-        fcpath('assets/jquery/jquery.min.js'), 
-        $event->refresh ? true : false
-    );
-    
-    $event->download(
-        'https://code.jquery.com/jquery-' . $config->version . '.js', 
-        fcpath('assets/jquery/jquery.js'), 
-        $event->refresh ? true : false
-    );
-});
+        $event->download(
+            'https://code.jquery.com/jquery-' . $config->version . '.min.js', 
+            fcpath('assets/jquery/jquery.min.js'), 
+            $event->refresh ? true : false
+        );
+        
+        $event->download(
+            'https://code.jquery.com/jquery-' . $config->version . '.js', 
+            fcpath('assets/jquery/jquery.js'), 
+            $event->refresh ? true : false
+        );
+    }
+);
